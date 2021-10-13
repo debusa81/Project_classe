91d33261f45ba7deb84ee467903feecf424c3cc2
John Doe <johndoe@example.com>
2021-10-13 08:38:34 +0200

    commit main  affichage constructeur

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("coucou");
+	Test testeur = new Test("alex",2,"Debs");
+
+    String mon_nom=testeur.setNom("Alexandre");
+    int mon_nombre=testeur.setNombre(5);
+    String mon_prenom=testeur.setPrenom("Debus");
+    System.out.println(mon_nom +" "+mon_nombre+" "+ mon_prenom);
     }
 }
